4f7a95f8abd81e5f8d11cd72e0b26a38d22aecb8
strajabot <strajabot@gmail.com>
2023-09-29 17:07:44 +0200

    Add more types

--- h/types.h
+++ h/types.h
@@ -1,11 +1,18 @@
-#ifndef STDINT_H
-#define STDINT_H
+#ifndef TYPES_H
+#define TYPES_H
 
 typedef unsigned long long uint64_t;
+typedef unsigned long uint32_t;
+typedef unsigned short uint16_t;
 typedef unsigned char uint8_t;
 typedef char bool_t;
 typedef uint64_t time_t;
 
+#define UINT64_MAX	0xFFFFFFFFFFFFFFFF
+#define UINT32_MAX	0xFFFFFFFF
+#define UINT16_MAX	0xFFFF
+#define UINT8_MAX	0xFF
+
 #define NULL 0
 
-#endif // !STDINT_H
+#endif // !TYPES_H
