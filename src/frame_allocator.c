b37beba205752160d2b1b694b89f64cc86fb8a31
strajabot <strajabot@gmail.com>
2023-10-30 23:09:31 +0100

    Add synchronization to frame allocator

--- src/frame_allocator.c
+++ src/frame_allocator.c
@@ -1,7 +1,11 @@
 #include "../h/frame_allocator.h"
 
-#include "../h/types.h"
 #include "../h/hw.h"
+#include "../h/spinlock.h"
+#include "../h/types.h"
+
+
+static uint32_t list_lock = 0;
 
 static free_frame_t* free_list = NULL;
 
@@ -12,6 +16,8 @@ static inline free_frame_t* next_frame(free_frame_t* frame)
 
 void frame_allocator_init()
 {
+	lock(&list_lock);
+	
 	free_list = (free_frame_t*) HEAP_START_ALIGNED;
 	free_list->next = NULL;
 
@@ -24,21 +30,37 @@ void frame_allocator_init()
 		next = next_frame(frame);
 	}
 	frame->next = NULL;
+
+	unlock(&list_lock);
 }
 
 void* frame_alloc()
 {
-	if(!free_list) return NULL;
+	lock(&list_lock);
+
+	if(!free_list) 
+	{
+		unlock(&list_lock);
+		return NULL;
+	}
 	free_frame_t* frame = free_list;
 	free_list = free_list->next;
+	
+	unlock(&list_lock);
+	
 	return frame;
 }
 
 void frame_free(void *ptr)
 {
 	ptr = PAGE_FLOOR(ptr);
+	
+	lock(&list_lock);
+
 	free_frame_t* frame = (free_frame_t*) ptr;
 	frame->next = free_list;
 	free_list = frame;
+
+	unlock(&list_lock);
 }
 
